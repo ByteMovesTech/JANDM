b707d38cf7a9881bd5902375cd9e4e30c23a7be5
ByteMovesTech <kcmike@gmail.com>
2026-04-08 20:23:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const questions = [
   {
     question: "Where did our story begin?",
-    answer: "tinder",
-    hint: "Think about how we met..."
+    answer: "reddit",
+    hint: "Think back to that very first place we connected online..."
   },
   {
     question: "What song is ours?",
@@ -10,31 +10,3 @@ const questions = [
     hint: "Aerosmith ❤️"
   }
 ];
-
-let current = 0;
-
-function loadQuestion() {
-  document.getElementById("question").innerText =
-    questions[current].question;
-}
-
-function submitAnswer() {
-  const input = document.getElementById("answer").value.toLowerCase();
-
-  if (input.includes(questions[current].answer)) {
-    current++;
-
-    if (current >= questions.length) {
-      window.location.href = "final.html";
-    } else {
-      loadQuestion();
-      document.getElementById("answer").value = "";
-      document.getElementById("hint").innerText = "You're getting closer ❤️";
-    }
-  } else {
-    document.getElementById("hint").innerText =
-      questions[current].hint;
-  }
-}
-
-loadQuestion();
